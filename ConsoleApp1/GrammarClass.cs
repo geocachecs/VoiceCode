221bdc50ffeb997b228f09ee5a77e37197cbd556
geocachecs <geocachecs>
2020-09-09 21:05:12 -0600

    added an abstract class to make things better

---
+++ ConsoleApp1/GrammarClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech.Recognition;
+
+
+namespace ConsoleApp2
+{
+    abstract class GrammarClass
+    {
+        protected static readonly object grammarLock = new object();
+
+        abstract public void HandleSpeechRecognized(object sender, SpeechRecognizedEventArgs e);
+        abstract public void ConfigureSRE(ref SpeechRecognitionEngine sre);
+        abstract public void UpdateSRE(ref SpeechRecognitionEngine sre);
+
+    }
+}
